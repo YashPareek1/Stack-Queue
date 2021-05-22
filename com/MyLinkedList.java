9668c43dbe7a8f390b46648862b2a728cbf5cd5f
YashPareek1 <yashpareekg4@gmail.com>
2021-05-22 23:37:02 +0530

    queue

--- com/MyLinkedList.java
+++ com/MyLinkedList.java
@@ -39,4 +39,15 @@ public class MyLinkedList<K extends Comparable<K>> {
         this.head = head.getNext();
         return tempNode;
     }
+    
+    //Enqueue Internally Calling append method on linkedlist
+    public void append(INode<K> MyNode) {
+        if(this.tail == null && this.head == null) {
+            this.tail = MyNode;
+            this.head = MyNode;
+        } else {
+            this.tail.setNext(MyNode);
+            this.tail = MyNode;
+        }
+    }
 }
